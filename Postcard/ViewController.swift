e847856f02f04071b0186323b026971953c98aa3
Stephen Chinnadorai <stephen@Stephens-MacBook-Pro.local>
2014-09-17 13:34:48 +0100

    Challenge 2

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,13 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
-
     @IBOutlet weak var messageLabel: UILabel!
-    
     @IBOutlet weak var enterNameTextField: UITextField!
-    
     @IBOutlet weak var enterMessageTextField: UITextField!
-    
     @IBOutlet weak var mailButton: UIButton!
     
     
